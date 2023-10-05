194ec6d2c65b85b264cbc8b55cd6447f36ba0517
tony1611 <antbarrantes@estudiantec.cr>
2023-10-04 22:39:12 -0600

    Initial file structure

---
+++ src/main/java/com/mycompany/gestorcomponentes/GestorComponentes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestorcomponentes;
+
+/**
+ *
+ * @author barra
+ * @author ayme
+ */
+public class GestorComponentes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
