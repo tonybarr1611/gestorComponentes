926a06f8cd98a89e8974a4e11b5d2f195cbc55ac
tony1611 <antbarrantes@estudiantec.cr>
2023-10-08 00:47:25 -0600

    Getter&Setter rango/alcance.
    Se agregan los Getter&Setter pendientes para los atributos de rango/alcance pertenecientes a los objetos de de Mediano alcance y choque/impacto.

--- src/main/java/com/mycompany/gestorcomponentes/Componentes/ChoqueImpacto.java
+++ src/main/java/com/mycompany/gestorcomponentes/Componentes/ChoqueImpacto.java
@@ -13,10 +13,20 @@ import java.util.ArrayList;
  * @author barra
  */
 public class ChoqueImpacto extends Componente{
-    int rango;
+    private int rango;
     
     public ChoqueImpacto(String nombre, String tipoApariencia, ArrayList<String> sprites, int vida, int cantidadGolpes, int nivel, int campos, int nivelAparicion, int rango){
         super(nombre, tipoApariencia, sprites, vida, cantidadGolpes, nivel, campos, nivelAparicion);
         this.rango = rango;
+    }
+
+    public int getRango() {
+        return rango;
+    }
+
+    public void setRango(int rango) {
+        if(rango > 0){
+            this.rango = rango;
+        }
     }
 }
